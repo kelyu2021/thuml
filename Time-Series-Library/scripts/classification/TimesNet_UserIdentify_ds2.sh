1c4d1504f883544bd697c53c6207e4fb40ca57a1
kelyu2021 <kelyu2021@gmail.com>
2024-03-29 12:30:00 -0400

    commit

---
+++ Time-Series-Library/scripts/classification/TimesNet_UserIdentify_ds2.sh
@@ -0,0 +1,163 @@
+export CUDA_VISIBLE_DEVICES=3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify0 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify1 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify2 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify3 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify4 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify5 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify6 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify7 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer \
+  --model_id UserIdentify8 \
+  --model TimesNet \
+  --data UEA \
+  --e_layers 2 \
+  --batch_size 16 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 2 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 30 \
+  --patience 10
