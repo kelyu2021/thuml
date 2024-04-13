027688a057b40b54546f85e565d59f7cbf2abf52
kelyu2021 <kelyu2021@gmail.com>
2024-04-13 10:34:03 -0400

    commit

--- Time-Series-Library/scripts/classification/TimesNet_UserIdentify_ds2.sh
+++ Time-Series-Library/scripts/classification/TimesNet_UserIdentify_ds2.sh
@@ -37,82 +37,82 @@ export CUDA_VISIBLE_DEVICES=3
 #   --train_epochs 30 \
 #   --patience 10 \
 #   > ./logs/dataset2_UserIdentify1.log
-
-python -u run.py \
-  --task_name classification \
-  --is_training 1 \
-  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
-  --model_id UserIdentify2 \
-  --model TimesNet \
-  --data UEA \
-  --e_layers 2 \
-  --batch_size 16 \
-  --d_model 32 \
-  --d_ff 32 \
-  --top_k 2 \
-  --des 'Exp' \
-  --itr 1 \
-  --learning_rate 0.001 \
-  --train_epochs 30 \
-  --patience 10 \
-  > ./logs/dataset2_UserIdentify2.log
-
-python -u run.py \
-  --task_name classification \
-  --is_training 1 \
-  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
-  --model_id UserIdentify3 \
-  --model TimesNet \
-  --data UEA \
-  --e_layers 2 \
-  --batch_size 16 \
-  --d_model 32 \
-  --d_ff 32 \
-  --top_k 2 \
-  --des 'Exp' \
-  --itr 1 \
-  --learning_rate 0.001 \
-  --train_epochs 30 \
-  --patience 10 \
-  > ./logs/dataset2_UserIdentify3.log
-
-python -u run.py \
-  --task_name classification \
-  --is_training 1 \
-  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
-  --model_id UserIdentify4 \
-  --model TimesNet \
-  --data UEA \
-  --e_layers 2 \
-  --batch_size 16 \
-  --d_model 32 \
-  --d_ff 32 \
-  --top_k 2 \
-  --des 'Exp' \
-  --itr 1 \
-  --learning_rate 0.001 \
-  --train_epochs 30 \
-  --patience 10 \
-  > ./logs/dataset2_UserIdentify4.log
-
-python -u run.py \
-  --task_name classification \
-  --is_training 1 \
-  --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
-  --model_id UserIdentify5 \
-  --model TimesNet \
-  --data UEA \
-  --e_layers 2 \
-  --batch_size 16 \
-  --d_model 32 \
-  --d_ff 32 \
-  --top_k 2 \
-  --des 'Exp' \
-  --itr 1 \
-  --learning_rate 0.001 \
-  --train_epochs 30 \
-  --patience 10 \
-  > ./logs/dataset2_UserIdentify5.log
+# 11:13 apr 12 owen
+# python -u run.py \
+#   --task_name classification \
+#   --is_training 1 \
+#   --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
+#   --model_id UserIdentify2 \
+#   --model TimesNet \
+#   --data UEA \
+#   --e_layers 2 \
+#   --batch_size 16 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 2 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --learning_rate 0.001 \
+#   --train_epochs 30 \
+#   --patience 10 \
+#   > ./logs/dataset2_UserIdentify2.log
+# 11:16 apr 12 Pitzer
+# python -u run.py \
+#   --task_name classification \
+#   --is_training 1 \
+#   --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
+#   --model_id UserIdentify3 \
+#   --model TimesNet \
+#   --data UEA \
+#   --e_layers 2 \
+#   --batch_size 16 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 2 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --learning_rate 0.001 \
+#   --train_epochs 30 \
+#   --patience 10 \
+#   > ./logs/dataset2_UserIdentify3.log
+# 11:16 apr 21 Pitzer
+# python -u run.py \
+#   --task_name classification \
+#   --is_training 1 \
+#   --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
+#   --model_id UserIdentify4 \
+#   --model TimesNet \
+#   --data UEA \
+#   --e_layers 2 \
+#   --batch_size 16 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 2 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --learning_rate 0.001 \
+#   --train_epochs 30 \
+#   --patience 10 \
+#   > ./logs/dataset2_UserIdentify4.log
+# 12:45 apr 13 owen
+# python -u run.py \
+#   --task_name classification \
+#   --is_training 1 \
+#   --root_path ./dataset/UserBehaviorsInSphericalVideoStreaming/transfer_10_10 \
+#   --model_id UserIdentify5 \
+#   --model TimesNet \
+#   --data UEA \
+#   --e_layers 2 \
+#   --batch_size 16 \
+#   --d_model 32 \
+#   --d_ff 32 \
+#   --top_k 2 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --learning_rate 0.001 \
+#   --train_epochs 30 \
+#   --patience 10 \
+#   > ./logs/dataset2_UserIdentify5.log
 
 python -u run.py \
   --task_name classification \
